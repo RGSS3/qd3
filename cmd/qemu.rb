d76144213d399cbdfe6b7904becad656de7b164d
you <you>
2020-04-24 13:10:37 +0800

    whpx qemu-send

--- cmd/qemu.rb
+++ cmd/qemu.rb
@@ -3,5 +3,19 @@ class QD3
 	def qemu(*args)
 		Kernel.system "#{ENV['QD3_EXE_QEMU']} #{args.join(' ')}"
 	end
+	
+	
+	desc "qemu-send ARGS", "(internal) send commands to the qemu for internal usage"
+	def qemu_send(*args)
+		port = qd3config["qemu"]["port"]
+		if port
+			require 'net/telnet'
+			r = Net::Telnet.new "Host"=>"localhost", 
+				                "Port"=>port,
+				                "Prompt"=>/\(qemu\)/,
+				                "Timeout"=>30
+		    print r.cmd(args.join(' '))				                
+		end
+	end
 end
 	
